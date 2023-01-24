3553a5398890e0bf2acaeec9ae2edb9400b6e8ce
MrSumada <ricksamericana.com>
2023-01-24 03:44:44 -0500

    add localstorage to ingredients

---
+++ src/components/Ingredient.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+const Ingredient = ({ recipe, i }) => {
+    
+    const storedChecked = JSON.parse(localStorage.getItem(`ingredient-${i}`));
+    const [boxChecked, setBoxChecked] = useState(false);
+
+    useEffect(()=>{
+        if (storedChecked === true) {
+            console.log("storedChecked: ", storedChecked)
+            setBoxChecked(storedChecked)
+        }
+        }, [])
+    
+    
+    
+    function handleChange(e) {
+        let isChecked = e.target.checked
+        
+        if (e.target.checked) {
+            console.log(e.target.id, " checked")
+            setBoxChecked(true);
+            localStorage.setItem(`ingredient-${i}`, true);
+        } else {
+            console.log(e.target.id, "unchecked")
+            setBoxChecked(false);
+            localStorage.setItem(`ingredient-${i}`, false);
+        }
+        
+    }
+
+    return (
+        <p key={i}>
+            <input type="checkbox" id={`ingredient-${i}`} 
+            checked={storedChecked}
+            onChange={e => handleChange(e)}
+            ></input>
+            <label for={`ingredient-${i}`}><span className="amount">{recipe.ingredients[i].quantity}</span>{recipe.ingredients[i].name}</label>
+        </p>
+    )
+}
+
+export default Ingredient;
--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,28 +1,11 @@
 import React, { useState } from "react";
+import Ingredient from "./Ingredient";
 
 const Ingredients = ({ recipe }) => {
-    
-    const [boxChecked, setBoxChecked] = useState(false);
-    
-    function handleChange(e) {
-        let isChecked = e.target.checked
-        
-        if (e.target.checked) {
-            console.log(e.target.id, " checked")
-            setBoxChecked(true);
-        } else {
-            console.log(e.target.id, "unchecked")
-            setBoxChecked(false);
-        }
-    }
 
     return (recipe.ingredients.map((ingredient, i) => 
         <p key={i}>
-            <input type="checkbox" id={`ingredient-${i}`} 
-            // checked={if (boxChecked) ? true : false} 
-            onChange={e => handleChange(e)}
-            ></input>
-            <label for={`ingredient-${i}`}><span className="amount">{recipe.ingredients[i].quantity}</span>{recipe.ingredients[i].name}</label>
+            <Ingredient recipe={recipe} i={i} />
         </p>
         )
     )
